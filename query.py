9056cb2f9c3cf29b6dd626cead15ae612095d573
ronghu <144379767+rhulucas@users.noreply.github.com>
2023-11-03 03:11:09 +0000

    feat: add menu
    add 4 option in the menu
    
    issue #3

--- query.py
+++ query.py
@@ -1,3 +1,9 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+print("q: quit")
+print("sn: search movie names")
+print("sc: search casts")
+print("list: print all the movie names")
+
