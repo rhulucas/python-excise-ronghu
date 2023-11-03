70a403f56563a318f33fba5f0a5a5f47ce99875c
ronghu <144379767+rhulucas@users.noreply.github.com>
2023-11-03 03:05:42 +0000

    feat: list all movies
    list all the movie's name when user inputs "list"
    
    issue #2

--- query.py
+++ query.py
@@ -1,3 +1,25 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+print("q: quit")
+print("sn: search movie names")
+print("sc: search casts")
+print("list: print all the movie names")
+
+user_input = input()
+while user_input != "q":
+
+    
+    if user_input == "list":
+        for movie in movies._movies:
+            print(movie['name'])
+    
+    print("q: quit")
+    print("sn: search movie names")
+    print("sc: search casts")
+    print("list: print all the movie names")
+    
+    user_input = input()
+  
+
