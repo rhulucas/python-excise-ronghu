ccbbd933f30c6f3a914d32d6ac75f09ee69f30f0
ronghu <144379767+rhulucas@users.noreply.github.com>
2023-11-03 02:59:23 +0000

    feat: update the query by movie name
    1. search movie name by case sensitive
    
    issue #1

--- query.py
+++ query.py
@@ -1,3 +1,31 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+print("q: quit")
+print("sn: search movie names")
+print("sc: search casts")
+print("list: print all the movie names")
+
+user_input = input()
+while user_input != "q":
+
+    
+    if user_input == "list":
+        for movie in movies._movies:
+            print(movie['name'])
+    elif user_input == "sn":
+        print("Enter a movie name:")
+        moviename_input = input ()
+        for movie in movies._movies:
+            if moviename_input in movie['name']:
+                print(movie['name'])
+        print(" ")
+    
+    print("q: quit")
+    print("sn: search movie names")
+    print("sc: search casts")
+    print("list: print all the movie names")
+    
+    user_input = input()
+  
